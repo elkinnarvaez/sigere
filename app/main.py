dc4432db5733741d37262ddbd5954910e240ed11
elkinnarvaez <elkinnarvaez2406@gmail.com>
2021-06-16 00:37:26 -0500

    Main modified

--- app/main.py
+++ app/main.py
@@ -4,19 +4,4 @@ app= Flask(__name__)
 
 @app.route('/')
 def index():
-  return "Ok"
-
-@app.route('/root')
-def root():
-  return "Elkin tiene novia"
-
-@app.route('/reyes')
-def reyes():
-  return "jejeje"
-
-@app.route('/elkin')
-def elkin():
-  return "Elkin"
-@app.route('/rengifo')
-def po():
-  return "Tengo un osito en el parqueadero"
+  return "Let's get started"
