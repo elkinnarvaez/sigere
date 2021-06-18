d397e6b5c73d1a88b6b97d03e0050bdacd864aa3
elkinnarvaez <elkinnarvaez2406@gmail.com>
2021-06-17 20:50:28 -0500

    test file

--- test.py
+++ test.py
@@ -3,14 +3,5 @@ from sys import stdin
 def main():
     return 0
 
-def foo():
-    return 0
-
-def testing1():
-    return 10
-
-def testing2():
-    return 100
-
 if __name__ == '__main__':
     main()
